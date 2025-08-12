b7575c9ff093e6a62ecda79e91420e3d8d6b7dcd
saiknght1 <84302348+saiknght1@users.noreply.github.com>
2025-08-12 23:55:47 +0530

    Update run.py

--- run.py
+++ run.py
@@ -4,15 +4,21 @@ import subprocess
 def run_bot():
     subprocess.run(["python", "bot.py"])
 
-def run_server():
-    subprocess.run(["python", "server.py"])
+def run_payment_server():
+    subprocess.run(["python", "payment_server.py"])
+
+def run_doctor_dashboard():
+    subprocess.run(["python", "doctor_dashboard.py"])
 
 if __name__ == "__main__":
     t1 = threading.Thread(target=run_bot)
-    t2 = threading.Thread(target=run_server)
+    t2 = threading.Thread(target=run_payment_server)
+    t3 = threading.Thread(target=run_doctor_dashboard)
 
     t1.start()
     t2.start()
+    t3.start()
 
     t1.join()
     t2.join()
+    t3.join()
